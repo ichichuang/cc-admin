20914ab946e1ae46f9a3944fabb31b1f98292b4a
ichichuang <chichuang134@gmail.com>
2025-07-28 16:04:19 +0800

    fix: 删除无效文件，优化全局css

--- src/api/index.ts
+++ src/api/index.ts
@@ -13,10 +13,8 @@ const apiModules = import.meta.glob('./modules/**/*.ts', { eager: true })
 const importedAPIs = autoImportModulesSync(apiModules)
 
 // 导出所有 API 模块（使用解构赋值方式）
-export const testAPI = importedAPIs.test
 export const authAPI = importedAPIs.auth
 export * from './modules/auth'
-export * from './modules/test'
 
 // 默认导出所有 API
 export default importedAPIs
