51b44e5a352bdad7c249bc6a3b234010e640c187
ichichuang <chichuang134@gmail.com>
2025-08-04 06:29:07 +0800

    fix: 为缺少版权注释的文件添加版权声明

---
+++ test-dayjs.js
@@ -0,0 +1,11 @@
+/**
+ * @copyright Copyright (c) 2025 chichuang
+ * @license 自定义商业限制许可证
+ * @description cc-admin 企业级后台管理框架 - dayjs 测试文件
+ *
+ * 本文件受版权保护，商业使用需要授权。
+ * 联系方式: https://github.com/ichichuang/cc-admin/issues
+ *
+ * This file is protected by copyright. Commercial use requires authorization.
+ * Contact: https://github.com/ichichuang/cc-admin/issues
+ */
